6f343c069cc5dc733def4408a32b38bf9b64f53c
LUBAC <yvan.lubac@euro-argo.eu>
2026-06-26 17:57:07 +0200

    Add amrit-mqtt-publisher as submodule

--- amrit-mqtt-publisher/src/amrit_mqtt_publisher/__init__.py
+++
@@ -1,7 +0,0 @@
-"""AMRIT MQTT Alert Publisher"""
-
-
-from .core.publisher import configure, publish_alert_file, publish_alert_message, publish_alert_messages
-from .models import Alert_raw, CloudEventAlertaRaw
-
-__all__ = ["Alert_raw", "CloudEventAlertaRaw", "configure", "publish_alert_file", "publish_alert_message", "publish_alert_messages"]
--- amrit-mqtt-publisher/src/amrit_mqtt_publisher/_mqtt/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Internal MQTT submodule.
-
-Contains internal classes and helpers for managing MQTT connections and publishing.
-Not part of the public API — may change without notice.
-"""
